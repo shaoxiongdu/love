0ac22c122d303df8abfc8398b9e796d0c22b2259
shaoxiongdu <email@shaoxiongdu.cn>
2021-10-06 17:00:58 +0800

    初始化

--- js/index.js
+++ js/index.js
@@ -24,48 +24,11 @@ setInterval(timer, 1000);
 
 /* 鼠标点击特效 */
 jQuery(document).ready(function($) {
+
     $("html").click(function(e) {
-        //随机选择文字
-        var clickWord = [
-            "❤",
-            "正能量",
-            "(*^▽^*)",
-            "元气满满",
-            "开心",
-            "快乐",
-            "可爱",
-            "暴富",
-            "暴瘦",
-            "❤",
-            "小赵考研上岸",
-            "去旅游",
-            "因为喜欢，可迎万难。",
-            "山水一程，三生有幸。",
-            "不偏不倚，刚好是你。",
-            "会陷入温柔暮色里 会陷入你",
-            "有你在身边 风都超级甜",
-            "入目无别人，四下皆是你",
-            "一起看日出到日落天气",
-            "永不妥协,光芒万丈",
-            "期待未来",
-            "灯火可亲，有梦可做。",
-            "未来可期",
-            "宝藏女孩",
-            "留下来一起生活",
-            "我们来日方长。",
-            "小小的日常。",
-            "保持炙热 坚持下去。",
-            "已建立长期合作伙伴关系",
-            "和你在一起，当下即永恒",
-            "小手一牵，岁岁年年",
-            "遇见即是上上签",
-            "保持公开，低调恋爱",
-            "好好生活，一直爱你",
-            "细水长流",
-            "快乐与平凡最浪漫",
-            "永远年轻，永远热泪盈眶。",
-            "今天的你也辛苦啦",
-        ];
+
+        //读取配置文件 初始化关键字数组
+        var clickWord = config.clickWord;
 
         var a_idx = Math.floor((Math.random() * clickWord.length));
         // 随机产生文字颜色
@@ -84,7 +47,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-family":"mmm",
-            "fontSize":Math.floor((Math.random() * 22)+15),
+            "fontSize":Math.floor((Math.random() * 15)+10),
             "font-weight": "bold",
             "color": "rgb("+color1+","+color2+","+color3+")",
             "-webkit-user-select":"none",
